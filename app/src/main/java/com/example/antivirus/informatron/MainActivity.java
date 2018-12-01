58468a789d660dfdf263f67e1b2f12f5ba20743a
Adnan <adnan.muhib@rocketmail.com>
2018-12-02 01:25:31 +0500

    :sweat_smile: Initialized Project with Main Activity

---
+++ app/src/main/java/com/example/antivirus/informatron/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.antivirus.informatron;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void GetDeviceInfo_clicked(View view){
+        try
+        {
+        Intent intent = new Intent(this, DeviceInfoActivity.class);
+        startActivity(intent);
+        }
+        catch (Exception e){
+            Log.e("Error",e.getMessage().toString());
+        }
+    }
+
+    public void GetContacts_clicked(View view){
+        try
+        {
+            Intent intent = new Intent(this, DeviceInfoActivity.class);
+            startActivity(intent);
+        }
+        catch (Exception e){
+            Log.e("Error",e.getMessage().toString());
+        }
+    }
+    public  void GetCallHistory_clicked(View view){
+        try
+        {
+            Intent intent = new Intent(this, DeviceInfoActivity.class);
+            startActivity(intent);
+        }
+        catch (Exception e){
+            Log.e("Error",e.getMessage().toString());
+        }
+    }
+
+    public  void GetMessages_clicked(View view){
+        try
+        {
+            Intent intent = new Intent(this, DeviceInfoActivity.class);
+            startActivity(intent);
+        }
+        catch (Exception e){
+            Log.e("Error",e.getMessage().toString());
+        }
+    }
+
+    public  void GetInstalledApps_clicked(View view){
+        try
+        {
+            Intent intent = new Intent(this, DeviceInfoActivity.class);
+            startActivity(intent);
+        }
+        catch (Exception e){
+            Log.e("Error",e.getMessage().toString());
+        }
+    }
+
+}
