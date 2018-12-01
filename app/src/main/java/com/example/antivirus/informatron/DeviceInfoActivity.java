a47205f9fb9c0b4e42bd51a58e598dd63fa5b4b4
Adnan <adnan.muhib@rocketmail.com>
2018-12-02 02:35:51 +0500

    :ok_hand: Update: Get IMEI of the Device

--- app/src/main/java/com/example/antivirus/informatron/DeviceInfoActivity.java
+++ app/src/main/java/com/example/antivirus/informatron/DeviceInfoActivity.java
@@ -1,8 +1,12 @@
 package com.example.antivirus.informatron;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,36 +19,35 @@ public class DeviceInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_device_info);
 
         List<HardwareObject> mObject = getHardwareInformation();
-        ListView androidList = (ListView)this.findViewById(R.id.lstViewInfo);
+        ListView androidList = (ListView) this.findViewById(R.id.lstViewInfo);
         HardwareAdapter hAdapter = new HardwareAdapter(this, mObject);
         androidList.setAdapter(hAdapter);
     }
-    private List<HardwareObject> getHardwareInformation(){
+
+    private List<HardwareObject> getHardwareInformation() {
+        String serviceName = this.TELEPHONY_SERVICE;
+        TelephonyManager m_telephonyManager = (TelephonyManager) getSystemService(serviceName);
+        String IMEI, IMSI;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // something todo
+
+        }
+        IMEI = m_telephonyManager.getDeviceId();
         List<HardwareObject> hardware = new ArrayList<HardwareObject>();
-        String[] mAndroid = getAndroidHardWare();
         hardware.add(new HardwareObject("TAGS", android.os.Build.TAGS));
         hardware.add(new HardwareObject("DEVICE", android.os.Build.DEVICE));
         hardware.add(new HardwareObject("BOARD", android.os.Build.BOARD));
-        hardware.add(new HardwareObject("SDK", mAndroid[0]));
-        hardware.add(new HardwareObject("Version", mAndroid[1]));
-        hardware.add(new HardwareObject("Brand", mAndroid[2]));
-        hardware.add(new HardwareObject("Manufacturer", mAndroid[3]));
-        hardware.add(new HardwareObject("Model", mAndroid[4]));
-        hardware.add(new HardwareObject("Display", mAndroid[5]));
-        hardware.add(new HardwareObject("Product", mAndroid[6]));
+        hardware.add(new HardwareObject("SDK", String.valueOf(Build.VERSION.SDK_INT)));
+        hardware.add(new HardwareObject("Version", Build.VERSION.RELEASE));
+        hardware.add(new HardwareObject("Brand", Build.BRAND));
+        hardware.add(new HardwareObject("Manufacturer", Build.MANUFACTURER));
+        hardware.add(new HardwareObject("Model", Build.MODEL));
+        hardware.add(new HardwareObject("Display", Build.DISPLAY));
+        hardware.add(new HardwareObject("Product", Build.PRODUCT));
         hardware.add(new HardwareObject("USER", android.os.Build.USER));
         hardware.add(new HardwareObject("TYPE", Build.TYPE));
         hardware.add(new HardwareObject("ID", Build.ID));
+        hardware.add(new HardwareObject("IMEI", IMEI));
         return hardware;
-    }
-    private String[] getAndroidHardWare(){
-        String androidSDK = String.valueOf(android.os.Build.VERSION.SDK_INT);
-        String androidVersion = android.os.Build.VERSION.RELEASE;
-        String androidBrand = android.os.Build.BRAND;
-        String androidManufacturer = android.os.Build.MANUFACTURER;
-        String androidModel = android.os.Build.MODEL;
-        String androidDisplay = android.os.Build.DISPLAY;
-        String androidProduct = android.os.Build.PRODUCT;
-        return new String[]{androidSDK, androidVersion, androidBrand, androidManufacturer, androidModel, androidDisplay, androidProduct};
     }
 }
